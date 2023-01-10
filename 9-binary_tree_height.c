bce2c80e885b05ec4b4de4ccb744712c9f1ee0f9
mavix21 <marcevizcarra2109@hotmail.com>
2023-01-10 18:22:11 -0500

    Fix: Indentation

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,12 +17,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	else
 	{
-		if (tree->left)
-		left_height = 1 + binary_tree_height(tree->left);
-		if (tree->right)
-		right_height = 1 + binary_tree_height(tree->right);
+		if (tree->left != NULL)
+			left_height = 1 + binary_tree_height(tree->left);
+
+		if (tree->right != NULL)
+			right_height = 1 + binary_tree_height(tree->right);
+
 		if (left_height > right_height)
 			return (left_height);
+
 		return (right_height);
 	}
 }

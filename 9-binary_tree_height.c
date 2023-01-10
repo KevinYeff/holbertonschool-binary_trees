05def6a9c16517a7b9a1f5b4b711f6711170610e
KevinYeff <kevinyeff94@gmail.com>
2023-01-10 17:52:44 -0500

    fix: betty style line 26/27 else is not necesary.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -23,7 +23,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		right_height = 1 + binary_tree_height(tree->right);
 		if (left_height > right_height)
 			return (left_height);
-		else
-		  return (right_height);
+		return (right_height);
 	}
 }

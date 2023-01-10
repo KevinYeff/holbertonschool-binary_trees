e29b7896a067b68a9fafc5c8184e9e854ae3aba3
KevinYeff <kevinyeff94@gmail.com>
2023-01-10 17:50:49 -0500

    fix: now displays the correct heigth of the trees.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,8 +8,8 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 	{
@@ -17,11 +17,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	else
 	{
-		left_height = binary_tree_height(tree->left);
-		right_height = binary_tree_height(tree->right);
-		if (left_height >= right_height)
-			return (left_height + 1);
+		if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+		if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+		if (left_height > right_height)
+			return (left_height);
 		else
-			return (right_height + 1);
+		  return (right_height);
 	}
 }

4a137911a10360a8608bf953acf87968e714b66f
KevinYeff <kevinyeff94@gmail.com>
2023-01-11 18:04:25 -0500

    update: Task 14 prototype added.

--- binary_trees.h
+++ binary_trees.h
@@ -78,4 +78,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 /* task 13 */
 size_t binary_tree_nodes(const binary_tree_t *tree);
 
+/* task 14 */
+int binary_tree_balance(const binary_tree_t *tree);
+
 #endif /* end of _BINARY_TREE_H_ */

eebefb014cfe2c497c97d94cd0c051b2c44525ba
mavix21 <marcevizcarra2109@hotmail.com>
2023-01-11 23:35:10 -0500

    Refactor

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,12 +9,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	else
-	{
-		if (tree && tree->left == NULL && tree->right == NULL)
-			return (1);
-
-		return (binary_tree_size(tree->left) + 1 +
-			binary_tree_size(tree->right));
-	}
+	return (binary_tree_size(tree->left) + 1 +
+		binary_tree_size(tree->right));
 }

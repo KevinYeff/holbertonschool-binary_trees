fe6a2a7c807b3693ccf9c23bfc2772769964c9d2
KevinYeff <kevinyeff94@gmail.com>
2023-01-11 20:15:16 -0500

    update: task 15 portotype added.

--- binary_trees.h
+++ binary_trees.h
@@ -81,4 +81,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 /* task 14 */
 int binary_tree_balance(const binary_tree_t *tree);
 
+/* task 15 */
+int binary_tree_is_full(const binary_tree_t *tree);
+
 #endif /* end of _BINARY_TREE_H_ */

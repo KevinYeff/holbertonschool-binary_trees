047f0ff94912837df9a3067c12a7cb8f8512e963
mavix21 <marcevizcarra2109@hotmail.com>
2023-01-12 22:53:55 -0500

    Else is not generally useful after a return

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -32,18 +32,16 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		if (tree->left != NULL)
-			left_height = 1 + binary_tree_height(tree->left);
 
-		if (tree->right != NULL)
-			right_height = 1 + binary_tree_height(tree->right);
+	if (tree->left != NULL)
+		left_height = 1 + binary_tree_height(tree->left);
 
-		balance = left_height - right_height;
+	if (tree->right != NULL)
+		right_height = 1 + binary_tree_height(tree->right);
 
-		return (balance);
-	}
+	balance = left_height - right_height;
+
+	return (balance);
 }
 
 /**
@@ -58,20 +56,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	else
-	{
-		if (tree->left != NULL)
-			left_height = 1 + binary_tree_height(tree->left);
 
-		if (tree->right != NULL)
-			right_height = 1 + binary_tree_height(tree->right);
+	if (tree->left != NULL)
+		left_height = 1 + binary_tree_height(tree->left);
 
-		if (left_height > right_height)
-			return (left_height);
+	if (tree->right != NULL)
+		right_height = 1 + binary_tree_height(tree->right);
 
-		return (right_height);
-	}
+	if (left_height > right_height)
+		return (left_height);
+
+	return (right_height);
 }
